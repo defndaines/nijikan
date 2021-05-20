8dee8cb6823ca512980964cc543db102dc726fc4
Michael Daines <953037+defndaines@users.noreply.github.com>
2021-05-20 14:37:10 -0700

    Just formatting

--- test/nijikan/core_test.clj
+++ test/nijikan/core_test.clj
@@ -8,15 +8,15 @@
               [0 0 1 0]
               [0 2 0 0]]]
     (testing "able to retrieve neighbors"
-    (let [senior-pos [1 2]]
-      (is (= 3 (get-in grid senior-pos))
-          "sanity test to ensure position is correct")
-      (is (= [1 1 1 1] (njk/neighbors grid senior-pos))
-          "expected four newborn neighbors"))
+      (let [senior-pos [1 2]]
+        (is (= 3 (get-in grid senior-pos))
+            "sanity test to ensure position is correct")
+        (is (= [1 1 1 1] (njk/neighbors grid senior-pos))
+            "expected four newborn neighbors"))
 
-    (let [adult-pos [3 1]]
-      (is (= 2 (get-in grid adult-pos)))
-      (is (= [1] (njk/neighbors grid adult-pos)))))))
+      (let [adult-pos [3 1]]
+        (is (= 2 (get-in grid adult-pos)))
+        (is (= [1] (njk/neighbors grid adult-pos)))))))
 
 (deftest age-test
   (testing "aging of empty cells"
