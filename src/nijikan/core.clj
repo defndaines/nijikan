cf55590f42c41e3f194817f6349665beeafc46d7
Michael Daines <953037+defndaines@users.noreply.github.com>
2021-05-20 13:24:41 -0700

    Add neighbor function
    Returns the values of all neighbors, treating 0 as empty.

--- src/nijikan/core.clj
+++ src/nijikan/core.clj
@@ -1,6 +1,12 @@
 (ns nijikan.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; 1 (newborn), 2 (adult), or 3 (senior).
+
+(defn neighbors [grid pos]
+  (let [[x y] pos]
+    (remove
+      (fn [v] (or (nil? v) (zero? v)))
+      (for [m [(dec x) x (inc x)]
+            n [(dec y) y (inc y)]
+            :when (not (and (= x m) (= y n)))]
+        (get-in grid [m n])))))
--- test/nijikan/core_test.clj
+++ test/nijikan/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer [deftest is testing]]
             [nijikan.core :as njk]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest neighbors-test
+  (let [grid [[0 1 0 0]
+              [0 1 3 1]
+              [0 0 1 0]
+              [0 2 0 0]]]
+    (testing "able to retrieve neighbors"
+    (let [senior-pos [1 2]]
+      (is (= 3 (get-in grid senior-pos))
+          "sanity test to ensure position is correct")
+      (is (= [1 1 1 1] (njk/neighbors grid senior-pos))
+          "expected four newborn neighbors"))
+
+    (let [adult-pos [3 1]]
+      (is (= 2 (get-in grid adult-pos)))
+      (is (= [1] (njk/neighbors grid adult-pos)))))))
