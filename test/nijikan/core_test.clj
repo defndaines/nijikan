28f2852fb70ceb8817ea4644fefca82b508d60b2
Michael Daines <953037+defndaines@users.noreply.github.com>
2021-05-20 13:02:37 -0700

    Initialize project (lein new)

---
+++ test/nijikan/core_test.clj
@@ -0,0 +1,7 @@
+(ns nijikan.core-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [nijikan.core :as njk]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
